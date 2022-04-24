63f226b424dbb9b4a13643012e57a5aa038e9fe5
Sanduni Wimalasiri <it20022792@my.sliit.lk>
2022-04-23 23:09:30 -0700

    Configuration of the Environment_A

---
+++ eclipse-workspace/PowerBillGeneratingSystem/src/main/java/com/example/PBGS/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package com.example.PBGS.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus
+public class ResourceNotFoundException extends RuntimeException{
+	
+	private static final long serialVersionUID =1L;
+	
+	public ResourceNotFoundException(String message){
+		super(message);
+	}
+
+}
