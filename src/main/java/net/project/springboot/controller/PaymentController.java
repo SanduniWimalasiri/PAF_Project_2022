30e6ea2de958cafb0d49ea4f8c990cbe9cc103da
Oshani Perera <86661668+IT20457020@users.noreply.github.com>
2022-04-25 21:20:28 +0530

    Third commit

--- src/main/java/net/project/springboot/controller/PaymentController.java
+++ src/main/java/net/project/springboot/controller/PaymentController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +36,13 @@ public class PaymentController {
 	public List<Payment> getAllPayments(){
 		return paymentRepository.findAll();
 	}
+	
+	//add payment details
+	@PostMapping("/payments")
+	public Payment makePayment (@RequestBody Payment payment) {
+		return paymentRepository.save(payment);
+	}
+	
 	
 	//get payment by id rest api
 	@GetMapping("/payments/{id}")
